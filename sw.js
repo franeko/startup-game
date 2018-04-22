258a6f965d1594daafa93bd6bb34087360310809
Franek Okrzesik <franeko@gmail.com>
2018-04-22 11:41:41 +0200

    Last SW

--- sw.js
+++ sw.js
@@ -20,7 +20,6 @@ const RUNTIME = 'runtime';
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   './',
-  '/',
   'index.html',
   'manifest.json',
   'lib/jquery.min.js',
